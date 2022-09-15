088a328d768eb888502eefaac4cd3dbf0e8c8175
Wraith-code <animashaunridwan478@gmail.com>
2022-09-14 19:41:48 -0700

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,28 @@
-#include "main.h"
+#include "main.h"                                                                                                                        
 
-/**
- * print_alphabet - print alphabet of letters
- *
- * Return: void
- *\
+                                                                                                                                         
 
- void print_alphabet(void)
- {
-           int i;
+/**                                                                                                                                      
 
-	   for (i = 'a'; i <= 'z'; i++)
+ * print_alphabet - print alphabet of letters                                                                                            
 
-	   {
-	               _putchar(i);
-           }
-	   _putchar('\n');
-	   }
+ *                                                                                                                                       
+
+ * Return: void                                                                                                                          
+
+ *\                                                                                                                                      
+
+                                                                                                                                         
+
+ void print_alphabet(void)                                                                                                               
+
+ {                                                                                                                                       
+
+           int i;                                                                                                                        
+
+                  for (i = 'a'; i <= 'z'; i++)
+{ 
+                       _putchar(i);
+}
+                     _putchar('\n');
+}
