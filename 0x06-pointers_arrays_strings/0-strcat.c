547db29e4e5e994bc95c393ce9960f752c71b1e4
Wraith-code <animashaunridwan478@gmail.com>
2022-09-23 18:57:52 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,22 @@
 #include "main.h"
 
 /**
- ** _strcat - function commute srtings
- * @dest: param pointer to a char
- * @src: param pointer to a char
- * Return: return value of dest
+ * strcat - Concatenates the string pointed to by @src, including the x 
+ *       null byte, to the end of the string pointed to by @dest.  
+ * @dest: A pointer to the string to be concatenated upon.  
+ * @src: The source string to be appended to @dest.  
+ *
+ * Return: A pointer to the destination string @dest.     
  */
-
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, const char *src)
 {
-	int i;
-	int j;
-	
-	i = 0;
-	j = 0;
+	 int index = 0, dest_len = 0; 
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+	 while (dest[index++])
+		 dest_len++;
 
-	dest[i] = '\0';
-	return (dest);
+	  for (index = 0; src[index]; index++)
+		  dest[dest_len++] = src[index];
+
+	  return (dest);
 }
